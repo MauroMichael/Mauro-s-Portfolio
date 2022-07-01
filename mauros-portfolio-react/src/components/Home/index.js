b14567c0f89bbb540ead08b5ab7bc603e65db523
MauroMichael <mmvec@hotmail.com>
2022-07-01 19:38:28 -0300

    third-deploy

--- mauros-portfolio-react/src/components/Home/index.js
+++ mauros-portfolio-react/src/components/Home/index.js
@@ -63,7 +63,7 @@ const Home = () => {
             rel="noopener noreferrer"
             download="MaurosResume.pdf"
           >
-            DOWNLOAD CV
+            DOWNLOAD MY CV
           </a>
       </div>
     </div>
